b6a6107e9349b7b8841030cd38f112d5a1467672
ashleyzawadi <zawadikibathi@gmail.com>
2019-08-26 12:03:15 +0300

    Add sign out links

--- src/Components/layout/Navbar.js
+++ src/Components/layout/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Link } from 'react-router-dom'
 import SignedInLinks from './SignedInLinks'
+import SignedOutLinks from './SignedOutLinks'
+
 
 const Navbar = () =>{
     return (
@@ -8,6 +10,7 @@ const Navbar = () =>{
             <div className="container">
                <Link to='/' className="brand-logo">ForeSight</Link>
                <SignedInLinks/>
+               <SignedOutLinks/>
             </div>
         </nav> 
     )
